d8af9072bc4603b89509392984b704d83268b6d4
Kevin-Estrada <ke81297@gmail.com>
2021-10-04 12:14:42 -0500

    Added some styling and layout to the main UI using bootstrap 5

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,12 @@
 </head>
 <body>
 
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+    </div>
+</nav>
+
 <div class="container">
     
     @yield('content')
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,47 @@
 
 @section('content')
     
-<h2>Inside a container</h2>
+<div class="row mt-5">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                Add Task
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="task">Task</label>
+                        <input type="text" name="task" id="task" placeholder="Task" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-5">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                View Tasks
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Task</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td>Task</td>
+                        <td><button class="btn btn-danger btn-sm">Delete</button></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
